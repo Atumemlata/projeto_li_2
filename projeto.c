4aed25a9c2f6b74c064b06984b00193a48342c57
a110547 <a110547@alunos.uminho.pt>
2025-04-04 11:52:31 +0100

    primeira

--- projeto.c
+++ projeto.c
@@ -52,52 +52,7 @@ int main() {
         scanf("%d %d %c", &linha, &coluna, &acao);
         
         if(linha < 0 || linha >= LINHAS || coluna < 0 || coluna >= COLUNAS) {
-            printf("Coorden#include <stdio.h>
-
-#define LINHAS 5
-#define COLUNAS 5
-
-char tabuleiro[LINHAS][COLUNAS] = {
-    {'e','c','a','d','c'},
-    {'d','c','d','e','c'},
-    {'b','d','d','c','e'},
-    {'c','d','e','e','b'},
-    {'a','c','c','b','b'}
-};
-
-void mostrar() {
-    printf("\n");
-    for(int i = 0; i < LINHAS; i++) {
-        for(int j = 0; j < COLUNAS; j++) {
-            printf("%c ", tabuleiro[i][j]);
-        }
-        printf("\n");
-    }
-    printf("\n");
-}
-
-int main() {
-    int linha, coluna;
-    char acao;
-
-    while(1) {
-        mostrar();
-        
-        printf("Digite (ex: 0 0 M para maiúscula ou 1 2 #): ");
-        scanf("%d %d %c", &linha, &coluna, &acao);
-        
-        if(linha < 0 || linha >= LINHAS || coluna < 0 || coluna >= COLUNAS) {
             printf("Coordenadas inválidas!\n");
-            continue;
-        }
-
-        if(acao == 'M') tabuleiro[linha][coluna] -= 32;
-        else if(acao == '#') tabuleiro[linha][coluna] = '#';
-        else printf("Ação inválida! Use M ou #\n");
-    }
-
-    return 0;
-}adas inválidas!\n");
             continue;
         }
 
