8681ffe507c4d8b9bab0d95d46a3dd976ca4ecc2
a110547 <a110547@alunos.uminho.pt>
2025-04-07 17:14:16 +0100

    7/4

--- projeto.c
+++ projeto.c
@@ -1,35 +1,40 @@
 #include <stdio.h>
 
-#define LINHAS 5
-#define COLUNAS 5
+#define LINHAS 26
+#define COLUNAS 26
 
 typedef struct {
     char original[LINHAS][COLUNAS];
     char atual[LINHAS][COLUNAS];
 } Jogo;
 
-// Matriz inicial pré-definida
-Jogo jogo = {
-    .original = {
-        {'e','c','a','d','c'},
-        {'d','c','d','e','c'},
-        {'b','d','d','c','e'},
-        {'c','d','e','e','b'},
-        {'a','c','c','b','b'}
-    },
-    .atual = {
-        {'e','c','a','d','c'},
-        {'d','c','d','e','c'},
-        {'b','d','d','c','e'},
-        {'c','d','e','e','b'},
-        {'a','c','c','b','b'}
+Jogo jogo;
+
+void carregar_matriz(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Erro ao abrir o arquivo\n");
+        return;
     }
-};
+
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
+            if (fscanf(file, " %c", &jogo.original[i][j]) != 1) {
+                printf("Erro ao ler o arquivo\n");
+                fclose(file);
+                return;
+            }
+            jogo.atual[i][j] = jogo.original[i][j];
+        }
+    }
+
+    fclose(file);
+}
 
 void mostrar() {
     printf("\n");
-    for(int i = 0; i < LINHAS; i++) {
-        for(int j = 0; j < COLUNAS; j++) {
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
             printf("%c ", jogo.atual[i][j]);
         }
         printf("\n");
@@ -45,27 +50,26 @@ int main() {
     int linha, coluna;
     char acao;
 
-    while(1) {
+    carregar_matriz("tabuleiro.txt");
+
+    while (1) {
         mostrar();
-        
+
         printf("Digite (ex: 0 0 M para maiúscula, 1 2 # riscar, 3 4 R resetar): ");
         scanf("%d %d %c", &linha, &coluna, &acao);
-        
-        if(linha < 0 || linha >= LINHAS || coluna < 0 || coluna >= COLUNAS) {
+
+        if (linha < 0 || linha >= LINHAS || coluna < 0 || coluna >= COLUNAS) {
             printf("Coordenadas inválidas!\n");
             continue;
         }
 
-        if(acao == 'M') { // Tornar maiúscula
+        if (acao == 'M') { // Tornar maiúscula
             jogo.atual[linha][coluna] = jogo.original[linha][coluna] - 32;
-        }
-        else if(acao == '#') { // Riscar
+        } else if (acao == '#') { // Riscar
             jogo.atual[linha][coluna] = '#';
-        }
-        else if(acao == 'R') { // Resetar para original
+        } else if (acao == 'R') { // Resetar para original
             resetar_celula(linha, coluna);
-        }
-        else {
+        } else {
             printf("Ação inválida! Use M, # ou R\n");
         }
     }
