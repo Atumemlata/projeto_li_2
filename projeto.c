6d848d4cc3a3528d46df5c9ba3490907e0d12799
a110547 <a110547@alunos.uminho.pt>
2025-04-08 14:09:43 +0100

    comentarios

--- projeto.c
+++ projeto.c
@@ -10,14 +10,14 @@ int main() {
     while (1) {
         comando = getchar();
 
-        if (comando == 's') break;
+        if (comando == 's') break;       // programa corre até pressionar a tecla s para sair 
 
-        if (comando == 'l') {
+        if (comando == 'l') {            // carrega o tabuleiro
             while (getchar() != '\n');
             if (carregar_matriz(&jogo, "tabuleiro.txt")) {
                 mostrar(&jogo);
             }
-        } else if (comando == 'b') {
+        } else if (comando == 'b') {     // Converte a letra da posição [linha][coluna] da matriz para maiúscula, usando a versão original.
             if (scanf("%d %d", &linha, &coluna) == 2) {
                 if (linha >= 0 && linha < jogo.linhas && coluna >= 0 && coluna < jogo.colunas) {
                     jogo.atual[linha][coluna] = maiuscula(jogo.original[linha][coluna]);
@@ -25,14 +25,14 @@ int main() {
                 }
             }
             while (getchar() != '\n');
-        } else if (comando == 'r') {
+        } else if (comando == 'r') {    // Converte a letra da posição [linha][coluna] da matriz para #, usando a versão original.
             if (scanf("%d %d", &linha, &coluna) == 2) {
                 if (linha >= 0 && linha < jogo.linhas && coluna >= 0 && coluna < jogo.colunas) {
                     jogo.atual[linha][coluna] = '#';
                     mostrar(&jogo);
                 }
             }
-            while (getchar() != '\n');
+            while (getchar() != '\n');  // limpa tudo o que não interessa ler 
         } else {
             while (getchar() != '\n');
         }
